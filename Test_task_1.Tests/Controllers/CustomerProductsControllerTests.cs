ab971705e47fc6d0f03916caa4892f9d364711ad
Hennadii <antiox129@gmail.com>
2023-09-22 20:33:32 +0200

    last

--- Test_task_1.Tests/Controllers/CustomerProductsControllerTests.cs
+++ Test_task_1.Tests/Controllers/CustomerProductsControllerTests.cs
@@ -1,18 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Test_task_1.Services;
-
-namespace Test_task_1.Tests.Controllers
-{
-    public class CustomerProductsControllerTests
-    {
-        private readonly MongoDBService _mongoDBService;
-        public CustomerProductsControllerTests(MongoDBService mongoDBService)
-        {
-            _mongoDBService = mongoDBService;
-        }
-    }
-}
+﻿
