2aaa4ce3eab88720efa6c3ffa954bab875cb410c
Hennadii <antiox129@gmail.com>
2023-09-25 00:47:41 +0200

    Uporządkowanie projektów

---
+++ Test_Task_1_Tests/Services/MongoDBServiceTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Test_task_1.Models;
+using Test_task_1.Services;
+using MongoDB.Bson;
+using MongoDB.Driver.Core.Clusters.ServerSelectors;
+
+namespace Test_task_1.Tests
+{
+    public class MongoDBServiceTests : IDisposable
+    {
+        private readonly MongoDBService _mongoDBService;
+
+        public MongoDBServiceTests()
+        {
+            // Deklaracja bazy testowej
+            var settings = new MongoDBSettings
+            {
+                ConnectionURI = "mongodb://localhost:27017",
+                DatabaseName = "test_task_1_tests",
+                CollectionNameCustomers = "customers",
+                CollectionNameOrders = "orders"
+            };
+            var mongoDBSettings = Options.Create(settings);
+            _mongoDBService = new MongoDBService(mongoDBSettings);
+        }
+
+        [Fact]
+        public async Task AddCustomer_ResultSuccessFindCustomerInDataBase()
+        {
+            // Arrange
+            var customer = new Customers // tworzenie klienta testowego
+            {
+                Customer_Name = "John",
+                Customer_Surname = "Doe",
+                Customer_Delivery_Address = "123 Main St"
+            };
+
+            // Act
+            await _mongoDBService.CreateAsync(customer); // dodajemy klienta testowego do bazy 
+
+            // Assert
+            var foundCustomer = await _mongoDBService.FindCustomerAsync(customer.Customer_Name, customer.Customer_Surname, customer.Customer_Delivery_Address); // sprawdzamy czy klient testowy istnieje w bazie
+            Assert.NotNull(foundCustomer); // zmienna nie może być pusta
+            Assert.Equal(customer.Customer_Name, foundCustomer.Customer_Name); // porównanie parametrów
+            Assert.Equal(customer.Customer_Surname, foundCustomer.Customer_Surname);
+            Assert.Equal(customer.Customer_Delivery_Address, foundCustomer.Customer_Delivery_Address);
+        }
+
+        [Fact]
+        public async Task AddOrderInDataBase_ResultSuccessFindOrderInDataBase()
+        {
+            // Arrange
+            var orders = new Orders // zamówienie testowe
+            {
+                Order_Name = "Koszulka",
+                Order_Description = "Koszulka wygodna polo club",
+                Order_CustomerId = "3253425346344534"
+            };
+
+            // Act
+            await _mongoDBService.CreateAsync(orders); // dodajemy zamówienie testowe do bazy
+
+            // Assert
+            var foundOrders = await _mongoDBService._ordersCollection
+                .Find(x => x.Order_Name == orders.Order_Name && x.Order_Description == orders.Order_Description && x.Order_CustomerId == orders.Order_CustomerId)
+                .FirstOrDefaultAsync(); // szukamy zamówienie w bazie danych
+            Assert.NotNull(foundOrders); // wartość nie może być pusta
+            Assert.Equal(orders.Order_Name, foundOrders.Order_Name); // porównujemy parametry otrzymanego zamówienia z bazy
+            Assert.Equal(orders.Order_Description, foundOrders.Order_Description);
+            Assert.Equal(orders.Order_CustomerId, foundOrders.Order_CustomerId);
+        }
+
+        public void Dispose() // czyścimy po testach baze 
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            client.DropDatabase("test_task_1_tests");
+        }
+    }
+}
---
+++ Test_task_1/Models/Customers.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace Test_task_1.Models;
+
+    public class Customers
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Customer_Id { get; set; } 
+        [BsonElement("customer_name")]
+        public string Customer_Name { get; set; } = null!;
+        [BsonElement("customer_surname")]
+        public string Customer_Surname { get; set; } = null!;
+        [BsonElement("customer_delivery_Address")]
+        public string Customer_Delivery_Address { get; set; } = null!;
+    }
+
