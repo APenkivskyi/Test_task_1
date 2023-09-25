5b32f70613486fe6199f66228c164b2a4471bf38
Hennadii <antiox129@gmail.com>
2023-09-25 23:37:00 +0200

    dopisanie testu CustomerProductsControllerTests : Post_ShouldNotAddCustomerIfItAlreadyExists

--- Test_Task_1_Tests/Controller/CustomerProductsControllerTests.cs
+++ Test_Task_1_Tests/Controller/CustomerProductsControllerTests.cs
@@ -41,8 +41,46 @@ namespace Test_Task_1_Tests.Controller
             // Sprawdzenie, czy wynik to OkObjectResult
             Assert.IsInstanceOf<OkObjectResult>(result);
 
-            // Sprawdzenie, czy metoda CreateAsync została wywołana z odpowiednimi danymi
+            // Sprawdzenie, czy metoda CreateAsync została wywołana raz dla klienta
             _mockMongoDBService.Verify(x => x.CreateAsync(It.IsAny<Customers>()), Times.Once);
+
+            // Sprawdzenie, czy metoda CreateAsync została wywołana raz dla zamówienia
+            _mockMongoDBService.Verify(x => x.CreateAsync(It.IsAny<Orders>()), Times.Once);
+        }
+        [Test]
+        public async Task Post_ShouldNotAddCustomerIfItAlreadyExists()
+        {
+            // Przygotowanie danych testowych
+            var customerName = "Adam";
+            var customerSurname = "Kowalski";
+            var customerDeliveryAddress = "Beach street";
+            var orderName = "Sample Order";
+            var orderDescription = "Sample Description";
+            var orderPrice = 100;
+
+            // Symulacja istniejącego klienta
+            var existingCustomer = new Customers
+            {
+                Customer_Name = customerName,
+                Customer_Surname = customerSurname,
+                Customer_Delivery_Address = customerDeliveryAddress,
+                Customer_Id = "2142352314321342"
+            };
+
+            _mockMongoDBService.Setup(x => x.FindCustomerAsync(customerName, customerSurname, customerDeliveryAddress))
+                .ReturnsAsync(existingCustomer); // Symulujemy istniejącego klienta
+
+            // Wywołanie metody Post
+            var result = await _controller.Post(customerName, customerSurname, customerDeliveryAddress, orderName, orderDescription, orderPrice);
+
+            // Sprawdzenie, czy wynik to OkObjectResult
+            Assert.IsInstanceOf<OkObjectResult>(result);
+
+            // Sprawdzenie, czy metoda CreateAsync dla klienta nie została wywołana
+            _mockMongoDBService.Verify(x => x.CreateAsync(It.IsAny<Customers>()), Times.Never);
+
+            // Sprawdzenie, czy metoda CreateAsync została wywołana raz dla zamówienia
+            _mockMongoDBService.Verify(x => x.CreateAsync(It.IsAny<Orders>()), Times.Once);
         }
     }
 }
