52b25cbf4a712f238ba717270d0c736f88ca4221
Hennadii <antiox129@gmail.com>
2023-10-04 14:57:20 +0200

    ghfh

--- Test_task_1/Program.cs
+++ Test_task_1/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDB"));
-builder.Services.AddSingleton<ICustomerRepository ,MongoCustomerRepository>();
+builder.Services.AddSingleton<MongoDBService>();
 
 var app = builder.Build();
 
